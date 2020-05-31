8c02f41248efb26c51b5ef777536475d5b58248a
zzkyeee <1090861587@qq.com>
2020-05-31 12:42:32 +0800

    28. 实现 strStr()

--- src/ImplementStrStr.java
+++ src/ImplementStrStr.java
@@ -1,40 +1,23 @@
-///**
-// * Created on 2020/5/28 11:45 PM
-// *
-// * @author zzkyeee
-// */
-//public class ImplementStrStr {
-//
-//    public int strStr(String haystack, String needle) {
-//        if (null == needle || needle.length() == 0) {
-//            return 0;
-//        }
-//        if (null == haystack || haystack.length() == 0) {
-//            return -1;
-//        }
-//        if (haystack.length() < needle.length()) {
-//            return -1;
-//        }
-//        int diff = haystack.length() - needle.length();
-//        for (int i = 0; i < diff; i++) {
-//            int j;
-//            for (j = 0; j < needle.length(); j++) {
-//                if (haystack.charAt(i + j) != needle.charAt(j)) {
-//                    break;
-//                }
-//            }
-//
-//            if (j == needle.length()) {
-//                return i;
-//            }
-//        }
-//
-//        return -1;
-//    }
-//
-//    public static void main(String[] args) {
-//        ImplementStrStr solution = new ImplementStrStr();
-//        System.out.println(solution.strStr("a", "a"));
-//    }
-//
-//}
+/**
+ * Created on 2020/5/28 11:45 PM
+ *
+ * @author zzkyeee
+ */
+public class ImplementStrStr {
+
+    public int strStr(String haystack, String needle) {
+        int l = needle.length();
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack.substring(i, i + l).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ImplementStrStr solution = new ImplementStrStr();
+        System.out.println(solution.strStr("hello", "ll"));
+    }
+
+}

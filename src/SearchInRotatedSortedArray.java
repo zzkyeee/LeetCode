515b14aab4407a988e9f44ef17c14befe760ac2a
zzkyeee <1090861587@qq.com>
2020-05-31 12:38:20 +0800

    33. 搜索旋转排序数组

---
+++ src/SearchInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+/**
+ * Created on 2020/5/31 12:08 PM
+ *
+ * @author zzkyeee
+ */
+public class SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[0] <= nums[mid]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray solution = new SearchInRotatedSortedArray();
+        System.out.println(solution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 7));
+    }
+}

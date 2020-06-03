8d473e952462b130608a5d41f8fe33ab475d7fe2
zzkyeee <1090861587@qq.com>
2020-06-03 23:32:59 +0800

    54. 螺旋矩阵

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -9,13 +9,46 @@ import java.util.List;
 public class SpiralMatrix {
 
     public List<Integer> spiralOrder(int[][] matrix) {
-        int i = 0, j = 0;
+        List<Integer> res = new LinkedList<>();
+        if (null == matrix || matrix.length == 0) {
+            return res;
+        }
         int m = matrix.length;
         int n = matrix[0].length;
-        List<Integer> res = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
-        int dir = 0;
-        return null;
+        // 圈数
+        int circle = 0;
+        int count = 0;
+        int x = 0, y = -1;
+        while (count < m * n) {
+            for (int i = 0; i < n - 2 * circle && count < m * n; i++) {
+                y++;
+                res.add(matrix[x][y]);
+                count++;
+            }
+            for (int i = 0; i < m - 1 - 2 * circle && count < m * n; i++) {
+                x++;
+                count++;
+                res.add(matrix[x][y]);
+            }
+            for (int i = 0; i < n - 1 - 2 * circle && count < m * n; i++) {
+                y--;
+                count++;
+                res.add(matrix[x][y]);
+            }
+            for (int i = 0; i < m - 2 - 2 * circle && count < m * n; i++) {
+                x--;
+                count++;
+                res.add(matrix[x][y]);
+            }
+            circle++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrix spiralMatrix = new SpiralMatrix();
+        spiralMatrix.spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
     }
 
 }
---
+++ src/SqrtX.java
@@ -0,0 +1,17 @@
+/**
+ * Created on 2020/6/3 1:00 AM
+ *
+ * @author zzkyeee
+ */
+public class SqrtX {
+
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+
+    public static void main(String[] args) {
+        SqrtX sqrtX = new SqrtX();
+        System.out.println(sqrtX.mySqrt(5));
+    }
+
+}

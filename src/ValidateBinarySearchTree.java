b54f2ca6d0cc1cd68d06899d0bf889083250573e
zzkyeee <1090861587@qq.com>
2020-06-12 23:05:13 +0800

    98. 验证二叉搜索树

---
+++ src/ValidateBinarySearchTree.java
@@ -0,0 +1,29 @@
+import po.TreeNode;
+
+/**
+ * Created on 2020/6/12 10:53 PM
+ *
+ * @author zzkyeee
+ */
+public class ValidateBinarySearchTree {
+
+    long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+
+        return isValidBST(root.right);
+    }
+
+}

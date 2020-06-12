e2d4cd388d2153eda06bf03bcbedd34811e7aa2e
zzkyeee <1090861587@qq.com>
2020-06-12 23:17:54 +0800

    101. 对称二叉树

---
+++ src/SymmetricTree.java
@@ -0,0 +1,25 @@
+import po.TreeNode;
+
+/**
+ * Created on 2020/6/12 11:06 PM
+ *
+ * @author zzkyeee
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return checkSame(root.left, root.right);
+    }
+
+    private boolean checkSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        return left != null && right != null && left.val == right.val
+                &&  checkSame(left.left, right.right) && checkSame(left.right, right.left);
+    }
+
+}

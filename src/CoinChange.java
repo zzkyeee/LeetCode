d133eaf1432b7ac087f83d6bebc0c3a5d830b89d
zzkyeee <1090861587@qq.com>
2020-08-31 08:38:29 +0800

    322. 零钱兑换

---
+++ src/CoinChange.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/**
+ * Created on 2020/8/31 8:35 AM
+ *
+ * @author zzkyeee
+ */
+public class CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+}
